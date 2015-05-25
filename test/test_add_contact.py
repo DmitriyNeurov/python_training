47d35619d4368a37343909424f8200042dd5ca91
DmitriyNeurov <dmitriy.neurov@mail.ru>
2015-05-25 22:34:30 +0300

    Созданы пакеты для разных зон ответственности

---
+++ test/test_add_contact.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from model.contact import Contact
+from fixture.application_contact import ApplicationContact
+
+
+@pytest.fixture
+def app(request):
+    fixture = ApplicationContact()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_contact(app):
+        app.login(username="admin", password="secret")
+        app.create_contact(Contact( firstname="Dmitriy", middlename="Dmitriy1", lastname="Neurov", nickname="DmitriyNeurov", title="Software tester", company="Very best",
+                            address="Russia, Moscow", home="+78955654", mobile="+74565456", work="+7456546456456", fax="+7454556",
+                            email2="dmitriy.neurovdmitriy1.@best", email3="dmitriy.neurovdmitriy1.@best",
+                            homepage="dmitriy.neurovdmitriy1.@best", byear="1986", address2="Russia, London", notes="gsgsgsfgssfgsfgs"))
+        app.logout()
+
+def test_add_empty_contact(app):
+        app.login(username="admin", password="secret")
+        app.create_contact(Contact( firstname="", middlename="", lastname="", nickname="", title="", company="",
+                            address="", home="", mobile="", work="", fax="",
+                            email2="", email3="",
+                            homepage="", byear="", address2="", notes=""))
+        app.logout()
+
---
+++ test/test_add_group.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from model.group import Group
+from fixture.application import Application
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_group(app):
+        app.login(username="admin", password="secret")
+        app.create_group(Group( name="PDT - Py, group 2", header="Test automation", footer="Each tester should be able to program :)"))
+        app.logout()
+
+
+def test_add_empty_group(app):
+        app.login(username="admin", password="secret")
+        app.create_group(Group(name="", header="", footer=""))
+        app.logout()
+
+

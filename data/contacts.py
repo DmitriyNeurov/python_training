ab12ebcd07e8754416a6ac15c3ef84089e641632
DmitriyNeurov <dmitriy.neurov@mail.ru>
2015-06-25 01:00:32 +0300

    Генераторы тестовых данных

---
+++ data/contacts.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+testdata = [Contact(firstname="jkl", middlename="dfg", lastname="fgh", nickname="asd", title="yjg", company="fghfgh", address="hjggj", home="453454",
+                    mobile="484245", work="445345", fax="44545", email2="fghgfh", email3="fhgfh", homepage="fghfgh", byear="fghfh", address2="fghfg", phone2="857587",
+                    notes="ghfgfh")]
